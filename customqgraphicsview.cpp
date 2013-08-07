5e68c64990ea9c03ff5bd0a96fd17ed102a577c4
Nick Conway <a.grinner@gmail.com>
2013-08-07 08:31:05 -0400

    actual content in customqgraphicsview now building

--- customqgraphicsview.cpp
+++ customqgraphicsview.cpp
@@ -1,15 +1,48 @@
 #include "customqgraphicsview.h"
 
+// Constant Non-integer Declarations
+// float type must not be initialized in the class declaration, but also NOT in
+// the header file.  Gotta be in the cpp. Per C++ standard
+const double CustomQGraphicsView::scale_limit_max = 3.0;
+const double CustomQGraphicsView::scale_limit_min = 0.41;
+const double CustomQGraphicsView::scale_up_rate = 0.01;
+const double CustomQGraphicsView::scale_down_rate = 0.01;
+// Keyboard panning
+const double CustomQGraphicsView::key_pan_delta_x = PATH_BASE_WIDTH * 21;
+const double CustomQGraphicsView::key_pan_delta_y = PATH_HELIX_HEIGHT + PATH_HELIX_PADDING/2;
+
+/* CONSTRUCTOR */
 CustomQGraphicsView::CustomQGraphicsView(QWidget *parent) :
     QGraphicsView(parent)
 {
-}
+    // set initial values of variable stuff
+    scale_size = 1.0;
+    scale_fit_factor = 1;
+    show_details = true;
+    scene_root_item = 0;
+    has_focus = false;
+    transform_enable = false;
+};
 
-CustomQGraphicsView::keyPressEvent(QKeyEvent *event) {
+qint32 CustomQGraphicsView::keyPanDeltaX(){
+    // TODO finish
+    return 1;
+};
+
+qint32 CustomQGraphicsView::keyPanDeltaY() {
+    // TODO finish
+    return 2;
+};
+
+void CustomQGraphicsView::zoomIn(qreal fractionofmax){
+    return;
+};
+
+void CustomQGraphicsView::keyPressEvent(QKeyEvent *event) {
     /*
     Handle key presses for mouse-drag transforms and arrow-key panning.
     */
-    if (!hasFocus) { // we don't have focus -> ignore keypress
+    if (!has_focus) { // we don't have focus -> ignore keypress
         return;
     }  
     switch(event->key()) {
@@ -18,16 +51,20 @@ CustomQGraphicsView::keyPressEvent(QKeyEvent *event) {
             QGraphicsView::keyPressEvent(event);
             break;
         case Qt::Key_Left:
-            scene_root_item->translate(keyPanDeltaX(), 0);
+            // scene_root_item->moveBy(keyPanDeltaX(), 0);
+            scene_root_item->setTransform(QTransform::fromTranslate(keyPanDeltaX(), 0), true);
             break;
         case Qt::Key_Up:
-            scene_root_item->translate(0, keyPanDeltaY());
+            // scene_root_item->moveBy(0, keyPanDeltaY());
+            scene_root_item->setTransform(QTransform::fromTranslate(0, keyPanDeltaY()), true);
             break;
         case Qt::Key_Right:
-            scene_root_item->translate(-keyPanDeltaX(), 0);
+            // scene_root_item->moveBy(-keyPanDeltaX(), 0);
+            scene_root_item->setTransform(QTransform::fromTranslate(-keyPanDeltaX(), 0), true);
             break;
         case Qt::Key_Down:
-            scene_root_item->translate(0, -keyPanDeltaY());
+            // scene_root_item->moveBy(0, -keyPanDeltaY());
+            scene_root_item->setTransform(QTransform::fromTranslate(0, -keyPanDeltaY()), true);
             break;
         case Qt::Key_Plus:
             zoomIn(0.3);
--- customqgraphicsview.h
+++ customqgraphicsview.h
@@ -7,6 +7,16 @@
 #include <QPainter>
 #include <QRectF>
 #include <QEvent>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QWheelEvent>
+#include <QGraphicsRectItem>
+#include <QTransform>
+
+#define PATH_BASE_WIDTH     10
+#define PATH_HELIX_HEIGHT    8
+#define PATH_HELIX_PADDING  12
 
 class CustomQGraphicsView : public QGraphicsView
 {
@@ -14,9 +24,80 @@ class CustomQGraphicsView : public QGraphicsView
 public:
     explicit CustomQGraphicsView(QWidget *parent = 0);
 
-    Qt::Alignment alignment() const;
-    void setAlignment(Qt::Alignment alignment);
-    
+protected:
+//    void focusInEvent(QFocusEvent *event);
+
+//    void focusOutEvent(QFocusEvent *event);
+
+    void keyPressEvent(QKeyEvent *event);
+
+//    void keyReleaseEvent(QKeyEvent *event);
+
+//    void enterEvent(QEvent * event);
+
+//    void leaveEvent(QEvent *);
+
+//    void mouseMoveEvent(QMouseEvent *event);
+
+//    void mousePressEvent(QMouseEvent *event);
+
+//    void mouseReleaseEvent(QMouseEvent *event);
+
+//    void wheelEvent(QWheelEvent *event);
+
+//    void paintEvent(QPaintEvent *event);
+
+private:
+    // constant floats to be initialize below by C++ standard
+    static const double scale_limit_max;
+    static const double scale_limit_min;
+    static const double scale_up_rate;
+    static const double scale_down_rate;
+    // Keyboard panning
+    static const double key_pan_delta_x;
+    static const double key_pan_delta_y;
+
+    qreal x0;
+    qreal y0;
+    double scale_size;
+    double scale_fit_factor;  // sets initial zoom level
+    qreal last_scale_factor;
+
+    bool show_details;
+    bool has_focus;
+    bool transform_enable;
+
+    QGraphicsRectItem *scene_root_item;  // the item to transform
+
+    // Modifier keys and buttons
+    static const int key_mod = Qt::Key_Control;
+    static const int button_pan = Qt::LeftButton;
+    static const int button_pan_alt = Qt::MidButton;
+    static const int button_zoom = Qt::RightButton;
+
+    //methods
+    void addToPressList(QObject *item);
+
+    qint32 keyPanDeltaX();
+
+    qint32 keyPanDeltaY();
+
+    void safeScale(qreal delta);
+
+    void zoomIn(qreal fractionofmax);
+
+    void zoomOut(qreal fractionofmin);
+
+    void dollyZoom(QMouseEvent *event);
+
+    void resetScale();
+
+    void zoomToFit();
+
+    void panEnable();
+
+    void panDisable();
+
     void setViewportUpdateOn(bool isEnabled);
 
     void activateSelection(bool isActive);
@@ -33,55 +114,13 @@ public:
 
     void allowGLSwitch();
 
-    void drawBackgroundGL(QPainter painter, QRectF rect);
+    void drawBackgroundGL(QPainter* painter, QRectF &rect);
 
-    void focusInEvent(QFocusEvent *event);
-
-    void focusOutEvent(QFocusEvent *event);
-
-    void setSelectionLock(selectionLock);
+    //    void setSelectionLock(selectionLock);
 
     void setScaleFitFactor(qreal value);
 
-    void setKeyPan(button);
-
-    void addToPressList(QObject *item);
-
-    qint32 keyPanDeltaX();
-
-    void keyPressEvent(QKeyEvent *event);
-
-    void keyReleaseEvent(QKeyEvent *event);
-
-    void enterEvent(QEvent * event);
-
-    void leaveEvent(QEvent *);
-
-    void mouseMoveEvent(QMouseEvent *event);
-
-    void mousePressEvent(QMouseEvent *event);
-
-    void mouseReleaseEvent(QMouseEvent *event);
-
-    void panEnable();
-
-    void panDisable();
-
-    void wheelEvent(QWheelEvent *event);
-
-    void safeScale(qreal delta);
-
-    void zoomIn(qreal fractionofmax);
-
-    void zoomOut(qreal fractionofmin);
-
-    void dollyZoom(QMouseEvent *event);
-
-    void resetScale();
-
-    void zoomToFit();
-
-    void paintEvent(QPaintEvent *event);
+    //    void setKeyPan(button);
 
 signals:
     
